1560fdaa40d62d47cbb3ba83c5c9bad887adf168
Wilkens Badio <wbadio693@gmail.com>
2023-11-21 17:04:31 -0500

    commit 2

--- stat_1.py
+++ stat_1.py
@@ -1,6 +1,18 @@
-from csvstat.csv import CSVStatsAnalyzer
+from csvstat.csv import  ExcelStatsAnalyzer
 if __name__ == "__main__":
 
-    filename = input("Antre chemen ou: ")
-analyzer = CSVStatsAnalyzer(filename)
-stats = analyzer.analyze_csv()
+    nom_fichier_excel =  input("Entrer votre chemin(path): ")
+analyseur_excel = ExcelStatsAnalyzer(nom_fichier_excel)
+
+
+resultat_excel = analyseur_excel.analyze_excel()
+
+
+donnees_chaines_excel = resultat_excel["strings"]
+donnees_floats_excel = resultat_excel["floats"]
+donnees_entiers_excel = resultat_excel["integers"]
+
+
+print("Données de chaînes de caractères :", donnees_chaines_excel)
+print("Données de nombres décimaux :", donnees_floats_excel)
+print("Données d'entiers :", donnees_entiers_excel)
