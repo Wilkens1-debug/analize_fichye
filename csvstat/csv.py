8e56030fd000fcb863af50b9f7c1242a09f43eec
Wilkens Badio <wbadio693@gmail.com>
2023-11-21 17:29:09 -0500

    comit10

--- csvstat/csv.py
+++ csvstat/csv.py
@@ -56,4 +56,3 @@ class ExcelStatsAnalyzer:
             return False
 
 
-
--- stat_1.py
+++ stat_1.py
@@ -16,3 +16,5 @@ donnees_entiers_excel = resultat_excel["integers"]
 print("Données de chaînes de caractères :", donnees_chaines_excel)
 print("Données de nombres décimaux :", donnees_floats_excel)
 print("Données d'entiers :", donnees_entiers_excel)
+
+print("")
